15c65d17d173d897653234f590ac83a01528c623
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-11-09 00:57:35 +0700

    Create level-set-segmentation.py

---
+++ level-set-segmentation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+from scipy.ndimage import gaussian_gradient_magnitude
+from skimage.measure import find_contours
+
+from image_function import *
+from metadata_function import *
+
+paths = getPath('Immunofluorescence images/')
+path = 'Immunofluorescence images/' + paths[0]
+img = cv2.imread(path, 0)
+cv2.imshow('Original', img)
+print(img.shape)
+plotImage(img, 'Original')
+
+img = clahe(img, clipLimit=8.0, tileGridSize=(8,8))
+plotImage(img, 'CLAHE')
+img = gaussianBlur(img, ksize=5, sigmaX=3)
+plotImage(img, 'Gaussian Blur')
+img = sharpening(img, 1)
+plotImage(img, 'Sharpening')
+
