8959f83aa6f5c8be0fda99a93000caf911e05f2a
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-11-12 03:05:30 +0700

    update deblur

---
+++ image_slicer.py
@@ -0,0 +1,24 @@
+# For slicer image in ratio
+import os
+import cv2
+from tqdm import tqdm
+from image_function import imgSlicer
+
+# Set path
+paths = 'Immunofluorescence images/DAPI'
+
+for file in tqdm(sorted(os.listdir(paths))):
+    path = paths + '/' + file
+    print(path)
+    img = cv2.imread(path, 1)
+    img_list = imgSlicer(img, type='d4')
+
+    init = 0
+    for each in img_list:
+        row = init // 4
+        col = init % 4
+        filename = file[:-4] + '_' + str(row) + '_' + str(col) + '.png'
+        cv2.imwrite('Dataset/DAPI/4x4/' + filename, each)
+        init += 1
+        if init == 16:
+            init = 0
